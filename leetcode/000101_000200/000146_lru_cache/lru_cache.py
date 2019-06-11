ceb75c5743cdc38a0ebf6077210d8860e8d61a96
WinnoChan <winnochan@icloud.com>
2019-06-11 11:12:29 +0800

    leetcode#146 init lru cache code

---
+++ leetcode/000101_000200/000146_lru_cache/lru_cache.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        return
+
+    def get(self, key: int) -> int:
+        return
+
+    def put(self, key: int, value: int) -> None:
+        return
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
