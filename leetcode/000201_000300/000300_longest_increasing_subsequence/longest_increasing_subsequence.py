b497cac1d46a8675b60fefcb70c4431843810652
WinnoChan <winnochan@icloud.com>
2019-06-12 15:25:59 +0800

    leetcode#300 init longest_increasing_subsequence code

---
+++ leetcode/000201_000300/000300_longest_increasing_subsequence/longest_increasing_subsequence.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        """
+        define dp()
+        """
+        return
